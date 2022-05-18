fb4289d175903e25c277a49b89d44052517e70bd
JakubVrch <vrchlabsky.jakub@gmail.com>
2022-05-18 11:05:17 +0000

    Create folder structure

--- client/accounting-notebook-client/jest.config.js
+++ client/accounting-notebook-client/jest.config.js
@@ -14,9 +14,9 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    "^@/components/(.*)$": "<rootDir>/components/$1",
-
-    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@components/(.*)$": "<rootDir>/common/components/$1",
+    "^@pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
