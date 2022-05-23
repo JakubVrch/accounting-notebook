024d87d25e33a94ded59b413c279105e43bcebad
JakubVrch <vrchlabsky.jakub@gmail.com>
2022-05-23 08:29:25 +0000

    add notestable page

---
+++ client/accounting-notebook-client/src/pages/notestable.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+//TODO: MUI table + install https://mui.com/material-ui/react-table/#collapsible-table
+
+const NotesTable: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Notes Table</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Notes Table</h1>
+      </main>
+    </div>
+  );
+};
+
+export default NotesTable;
