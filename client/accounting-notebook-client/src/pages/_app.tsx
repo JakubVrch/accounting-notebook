58be1fe86a874f8230ca5eb588dbc86cdb223cd4
JakubVrch <vrchlabsky.jakub@gmail.com>
2022-06-27 15:56:14 +0000

    Implement react query with fetch (temporary)

--- client/accounting-notebook-client/src/pages/_app.tsx
+++ client/accounting-notebook-client/src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   require("../mocks");
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
--- client/accounting-notebook-client/src/pages/notestable.tsx
+++ client/accounting-notebook-client/src/pages/notestable.tsx
@@ -1,14 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { TransactionTable, mockTransactions } from "@modules/TransactionTable";
-import { useEffect, useState } from "react";
+import { TransactionTable, ITransaction } from "@modules/TransactionTable";
+import { useQuery } from "react-query";
 
 //TODO: MUI table + install https://mui.com/material-ui/react-table/#collapsible-table
 
 const NotesTable: NextPage = () => {
   //TODO: Call transactions from API
-  const [transactions, setTransactions] = useState<any>(null);
-  useEffect(() => setTransactions(mockTransactions), []);
+  const getTransactions = async () => {
+    const res = await fetch("https://my.backend/test");
+    const transactions = await res.json();
+    transactions.forEach((element: { date: string | number | Date }) => {
+      element.date = new Date(element.date);
+    });
+    return transactions;
+  };
+
+  const transactionQuery = useQuery("transactions", getTransactions);
 
   return (
     <div>
@@ -19,7 +27,10 @@ const NotesTable: NextPage = () => {
 
       <main>
         <h1>Notes Table</h1>
-        {transactions ? <TransactionTable transactions={transactions} /> : null}
+        <button onClick={() => transactionQuery.refetch()}>Load test</button>
+        {transactionQuery.data ? (
+          <TransactionTable transactions={transactionQuery.data} />
+        ) : null}
       </main>
     </div>
   );
