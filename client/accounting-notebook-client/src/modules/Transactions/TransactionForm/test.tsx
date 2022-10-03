8a6042afb70a966dd5d46fb7997cfa79cb611535
JakubVrch <vrchlabsky.jakub@gmail.com>
2022-10-03 13:56:51 +0000

    refactor form tests

--- client/accounting-notebook-client/src/modules/Transactions/TransactionForm/test.tsx
+++ client/accounting-notebook-client/src/modules/Transactions/TransactionForm/test.tsx
@@ -1,91 +1,71 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import New from "pages/new";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { FormValues, TransactionForm } from "..";
 
+const setup = (onSubmit: Parameters<typeof TransactionForm>[0]["onSubmit"]) => {
+  const form = render(
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <TransactionForm onSubmit={onSubmit} />
+    </LocalizationProvider>
+  );
+  return {
+    dateField: form.getByRole("textbox", { name: "Date *" }),
+    noteField: form.getByRole("textbox", { name: "Note *" }),
+    accountFields: form.getAllByRole("textbox", { name: "Account *" }),
+    amountFields: form.getAllByRole("textbox", { name: "Amount *" }),
+    lineNoteFields: form.getAllByRole("textbox", { name: "Line note" }),
+    submitButton: form.getByRole("button", { name: "Submit" }),
+    ...form,
+  };
+};
+
 describe("TransactionForm", () => {
   it("renders fields", () => {
-    render(
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <TransactionForm onSubmit={() => null} />
-      </LocalizationProvider>
-    );
-
-    expect(screen.getByRole("textbox", { name: "Date *" })).toBeInTheDocument();
-    expect(screen.getByRole("textbox", { name: "Note *" })).toBeInTheDocument();
-    expect(screen.getAllByRole("textbox", { name: "Account *" })).toHaveLength(
-      2
-    );
-    expect(screen.getAllByRole("textbox", { name: "Amount *" })).toHaveLength(
-      2
-    );
-    expect(screen.getAllByRole("textbox", { name: "Line note" })).toHaveLength(
-      2
-    );
-    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
+    const form = setup(() => null);
+    expect(form.dateField).toBeInTheDocument();
+    expect(form.noteField).toBeInTheDocument();
+    expect(form.accountFields).toHaveLength(2);
+    expect(form.amountFields).toHaveLength(2);
+    expect(form.lineNoteFields).toHaveLength(2);
+    expect(form.submitButton).toBeInTheDocument();
   });
 
   it("can submit form", async () => {
-    const user = userEvent.setup({ skipClick: true });
+    const { clear, click, type } = userEvent.setup();
     const onSubmit = jest.fn();
+    const form = setup(onSubmit);
 
-    render(
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <TransactionForm onSubmit={onSubmit} />
-      </LocalizationProvider>
-    );
+    await clear(form.dateField);
+    await type(form.dateField, "2022-05-12");
+    await type(form.noteField, "Test Text 123");
+    await type(form.accountFields[0], "Test Text 123");
+    await type(form.amountFields[0], "123,53");
+    await type(form.lineNoteFields[0], "Test Text 123");
+    await type(form.accountFields[1], "Test Text 456");
+    await type(form.amountFields[1], "89");
 
-    await userEvent.clear(screen.getByRole("textbox", { name: "Date *" }));
-
-    await userEvent.type(
-      screen.getByRole("textbox", { name: "Date *" }),
-      "2022-05-12"
+    await click(form.submitButton);
+    expect(onSubmit).toBeCalledWith(
+      {
+        entries: [
+          {
+            value: "123,53",
+            account: "Test Text 123",
+            note: "Test Text 123",
+          },
+          {
+            value: "89",
+            account: "Test Text 456",
+            note: "",
+          },
+        ],
+        date: new Date("2022-05-12"),
+        note: "Test Text 123",
+      },
+      expect.anything()
     );
-    await userEvent.type(
-      screen.getByRole("textbox", { name: "Note *" }),
-      "Test Text 123"
-    );
-    await userEvent.type(
-      screen.getAllByRole("textbox", { name: "Account *" })[0],
-      "Test Text 123"
-    );
-    await userEvent.type(
-      screen.getAllByRole("textbox", { name: "Amount *" })[0],
-      "123,53"
-    );
-    await userEvent.type(
-      screen.getAllByRole("textbox", { name: "Line note" })[0],
-      "Test Text 123"
-    );
-    await userEvent.type(
-      screen.getAllByRole("textbox", { name: "Account *" })[1],
-      "Test Text 456"
-    );
-    await userEvent.type(
-      screen.getAllByRole("textbox", { name: "Amount *" })[1],
-      "89"
-    );
-
-    await user.click(screen.getByRole("button", { name: "Submit" }));
-
-    expect(onSubmit.mock.calls[0][0]).toEqual({
-      entries: [
-        {
-          value: "123,53",
-          account: "Test Text 123",
-          note: "Test Text 123",
-        },
-        {
-          value: "89",
-          account: "Test Text 456",
-          note: "",
-        },
-      ],
-      date: new Date("2022-05-12"),
-      note: "Test Text 123",
-    });
   });
   //TOOD: write test for required errors
   it.todo("provides error for required fields");
